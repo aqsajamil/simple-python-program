f71d67238f341ed914e5ff54d6e12c9e1173de3f
aqsa <aqsajamil476@gmail.com>
2018-08-05 09:54:22 -0700

    comment

--- ex1.py
+++ ex1.py
@@ -3,3 +3,11 @@
 print("helo world")
 print("i like typing this")
 print("this is fun")
+
+
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python. also called octothrope
+print ("I like to this program")
+
+
+print ("it will run")
